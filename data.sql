5ededd03d3c2f527ca016fcf51187848df39d5b5
George Magdy <georgtriple@gmail.com>
2022-02-14 20:08:24 +0200

    Update animal name

--- data.sql
+++ data.sql
@@ -2,4 +2,4 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 VALUES (1, 'Agumon', '2020-02-03', 0, false, 10.23),
        (2, 'Gabumon', '2018-11-15', 2, true, 8),
        (3, 'Pikachu', '2021-01-07', 1, true, 15.04), 
-       (4, 'Gabumon', '2017-05-12', 5, true, 11);
+       (4, 'Devimon', '2017-05-12', 5, true, 11);
