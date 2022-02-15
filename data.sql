d91f926f49f1dcbe3f1def44734285b43d8a2dbe
George Magdy <georgtriple@gmail.com>
2022-02-15 19:42:38 +0200

    Insert Charmander data

--- data.sql
+++ data.sql
@@ -2,4 +2,5 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Agumon', '2020-02-03', 0, false, 10.23),
 	('Gabumon', '2018-11-15', 2, true, 8),
 	('Pikachu', '2021-01-07', 1, true, 15.04), 
-	('Devimon', '2017-05-12', 5, true, 11);
+	('Devimon', '2017-05-12', 5, true, 11),
+	('Charmander', '2020-02-08', 0, false, -11);
