56b493a3a98fdfa7dc13f187824edf22703a83c7
George Magdy <georgtriple@gmail.com>
2022-02-16 19:37:06 +0200

    Update bob data

--- data.sql
+++ data.sql
@@ -18,7 +18,7 @@ INSERT INTO owners (full_name, age)
 VALUES 
 	('Sam Smith', 34),
 	('Jennifer Orwell', 19),
-	('Bob', 19),
+	('Bob', 45),
 	('Melody Pond', 77),
 	('Dean Winchester', 14),
 	('Jodie Whittaker', 38);
