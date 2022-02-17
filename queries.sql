77e865b425ed476570e86c1e83909eff62ffad9b
George Magdy <georgtriple@gmail.com>
2022-02-17 19:33:48 +0200

    Add select all vets and their specialties

--- queries.sql
+++ queries.sql
@@ -151,3 +151,12 @@ ON animals.id = visits.animals_id
 INNER JOIN vets
 ON visits.vets_id = vets.id
 WHERE vets.name = 'Stephanie Mendez'; 
+
+-- List all vets and their specialties, including vets with no specialties
+SELECT vets.name AS "Vets Name",
+species.name AS "Species Name"
+FROM vets
+LEFT JOIN specializations
+ON vets.id = specializations.vets_id
+LEFT JOIN species
+ON species.id = specializations.species_id; 
