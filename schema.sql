6fb9c8fc076f50cfc03d978245148d5160f20828
George Magdy <georgtriple@gmail.com>
2022-02-21 23:36:40 +0200

    Create index for vets_id

--- schema.sql
+++ schema.sql
@@ -61,3 +61,5 @@ CREATE TABLE visits (
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
 CREATE INDEX animals_visits_idx ON visits(animals_id);
+
+CREATE INDEX vets_visits_idx ON visits(vets_id);
