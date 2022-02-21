17602bfd0d188213293fc5100b63af4ebf9d34a9
ShadyShawkat <shadyshawkat91@gmail.com>
2022-02-21 23:38:03 +0200

    Create index for email column in owners table

--- schema.sql
+++ schema.sql
@@ -63,3 +63,5 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 CREATE INDEX animals_visits_idx ON visits(animals_id);
 
 CREATE INDEX vets_visits_idx ON visits(vets_id);
+
+CREATE INDEX owners_visits_idx ON owners(email);
