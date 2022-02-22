671287c77e12c545079c2b23a410d0f6ca967dbd
George Magdy <georgtriple@gmail.com>
2022-02-22 18:11:09 +0200

    Add invoice_items table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -33,3 +33,15 @@ CREATE TABLE invoices
 	medical_history_id INT,
 	FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+CREATE TABLE invoice_items
+(
+	id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoice_id INT,
+	treatment_id INT,
+	FOREIGN KEY (invoice_id) REFERENCES invoices(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	FOREIGN KEY (treatment_id) REFERENCES treatments(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
